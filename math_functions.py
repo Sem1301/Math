23b7579a88d433c23e71b2573d7c86842528545d
Sem1301 <sem@ivonet.nl>
2021-05-15 18:36:24 +0200

    with test case 5000 loops compare performance

---
+++ math_functions.py
@@ -0,0 +1,7 @@
+
+
+def product_list(numbers: list) -> int:
+    total = 1
+    for num in numbers:
+        total *= num
+    return total
--- prime_factorization.py
+++ prime_factorization.py
@@ -1,24 +1,12 @@
 import pickle
-from math import *
 import time
 
-from primes_generator import is_prime
-
 PRIMES = pickle.load(open("primes_1m.pickle", "rb"))
-
-
-def product_factors(factors: list) -> int:
-    total = 1
-    for factor in factors:
-        total *= factor
-    # print("product:", total, factor)
-    return total
 
 
 def prime_factor(n: int) -> list:
     """There is no optimization yet, so large number take a very long time to compute
     """
-
     idx = 0
     i = PRIMES[idx]
     factors = []
@@ -35,50 +23,7 @@ def prime_factor(n: int) -> list:
     return factors
 
 
-def prime_factor_Sem(n: int) -> list:
-    """There is no optimization yet, so large number take a very long time to compute
-    """
-    orig = n
-    if is_prime(n):
-        return [n, ]
-    idx = 0
-    i = PRIMES[idx]
-    factors = []
-    while i <= n:
-        if n < 2 * i:
-            factors.append(int(n))
-            if product_factors(factors) == orig:
-                return factors
-            i = n
-        elif n % i == 0:
-            n /= i
-            factors.append(i)
-        else:
-            idx += 1
-            i = PRIMES[idx]
-    return factors
-
-
-def prime_factors_2(n):
-    """Returns all the prime factors of a positive integer
-    used in "opgave 22" of 2017
-    """
-    factors = []
-    idx = 0
-    d = PRIMES[idx]
-    while n > 1:
-        while n % d == 0:
-            factors.append(d)
-            n /= d
-        d += 1
-        if d * d > n:
-            if n > 1:
-                factors.append(int(n))
-            break
-    return factors
-
-
-def prime_factors_3(n):
+def prime_factors_ivo(n):
     """Returns all the prime factors of a positive integer
     used in "opgave 22" of 2017
     """
@@ -101,24 +46,23 @@ def prime_factors_3(n):
 if __name__ == '__main__':
     # print(prime_factor(int(input('input your integer value here: '))))
 
-    print("=" * 80)
-    print("Sem")
-    print("=" * 80)
-    start = time.time()
-    for i in range(201, 5000, 3):
-        # print(i, prime_factor(i))
-        prime_factor(i)
-    print("Sem:", time.time() - start)
-    print("=" * 80)
-    print("Ivo")
-    print("=" * 80)
-    start = time.time()
-    for i in range(201, 5000, 3):
-        # print(prime_factors_2(i))
-        prime_factors_2(i)
-    print("Ivo2:", time.time() - start)
-    start = time.time()
-    for i in range(201, 5000, 3):
-        # print(i, prime_factors_2(i))
-        prime_factors_3(i)
-    print("Ivo3:", time.time() - start)
+    sem = []
+    ivo = []
+    for i in range(5000):
+        if i % 50 == 0:
+            print("Iteration:", i)
+
+        start = time.time()
+        for i in range(201, 5000, 3):
+            prime_factors_ivo(i)
+        ivo_time = time.time() - start
+        ivo.append(ivo_time)
+
+        start = time.time()
+        for i in range(201, 5000, 3):
+            prime_factor(i)
+        sem_time = time.time() - start
+        sem.append(sem_time)
+
+    print("Sem:", sum(sem) / len(sem))
+    print("Ivo:", sum(ivo) / len(ivo))
