45522d9ecd703205d80bdefdd0c2431a5d8774bb
Sem1301 <sem@ivonet.nl>
2021-05-21 21:19:00 +0200

    Added 'interval version' of collatz in math_function Collatz function work in progress

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,18 +1,37 @@
-def collatz(a, b):
+def collatz1(a, b):
     """This collatz conjecture script shows the amount of steps needed,
-    for a certain interval of numbers to get to 1: (number(n), steps(s))
+    for a certain interval of integers to get to 1: (integer(i), steps(s))
+    It only works for positive integers {1, 2, 3, ...}
     """
-    for n in range(a, b + 1):
+    if a < 1:
+        a = 1
+    for i in range(a, b + 1):
         y = 0
-        x = n
-        while n != 1:
+        x = i
+        while i != 1:
             y += 1
-            if n % 2 == 0:
-                n /= 2
+            if i % 2 == 0:
+                i /= 2
             else:
-                n = 3 * n + 1
-        print('(' + str(x) + ', ' + str(y) + ')')
+                i = 3 * i + 1
+        yield x, y
+
+
+def collatz(value):
+    """This collatz conjecture script shows the amount of steps needed,
+    for a certain interval of integers to get to 1: (integer(i), steps(s))
+    It only works for positive integers {1, 2, 3, ...}
+    """
+    steps = 0
+    i = value
+    while i != 1:
+        steps += 1
+        if i % 2 == 0:
+            i /= 2
+        else:
+            i = 3 * i + 1
+    return steps
 
 
 if __name__ == '__main__':
-    collatz(1, 100)
+    [print("Value: %5s -> Steps: %s" % (x, collatz(x))) for x in range(1, 101)]
