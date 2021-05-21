e68de7d5d2a4dc4c38d20946f22559b96cc82d5f
Sem1301 <sem@ivonet.nl>
2021-05-21 22:59:04 +0200

    generating plots

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,27 +1,16 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+COLLATZ_PATH = os.path.join(os.path.split(__file__)[0], "collatz_plots")
+try:
+    os.mkdir(COLLATZ_PATH)
+except IOError:
+    pass
+
 
 # from math_functions import collatz
-
-
-def collatz1(value):
-    """TODO doc
-    This collatz conjecture script shows the amount of steps needed,
-    for a certain interval of integers to get to 1: (integer(i), steps(s))
-    It only works for positive integers {1, 2, 3, ...}
-    """
-    steps = 0
-    i = value
-    while i != 1:
-        steps += 1
-        if i % 2 == 0:
-            i /= 2
-            yield steps, "left", int(i)
-        else:
-            i = 3 * i + 1
-            yield steps, "right", int(i)
-
 
 def collatz(value):
     """TODO doc
@@ -29,16 +18,13 @@ def collatz(value):
     for a certain interval of integers to get to 1: (integer(i), steps(s))
     It only works for positive integers {1, 2, 3, ...}
     """
-    steps = 0
     i = value
     while i != 1:
-        steps += 1
         if i % 2 == 0:
             i /= 2
-            yield steps, "left", int(i)
         else:
             i = 3 * i + 1
-            yield steps, "right", int(i)
+        yield int(i)
 
 
 def collatz_generator(start=1):
@@ -48,19 +34,23 @@ def collatz_generator(start=1):
         num += 1
 
 
-def collatz_plotter(min_value=1, max_value=100, dx=1):
-    xas = np.arange(min_value, max_value, dx)
-    yas = [collatz(x) for x in range(min_value, max_value)]
+def collatz_plotter():
+    for item in range(2, 100):
+        yas = []
+        for idx, y in enumerate(collatz(item), start=1):
+            yas.append(y)
+        xas = np.arange(1, len(yas) + 1, 1)
 
-    fig, ax = plt.subplots()
-    ax.plot(xas, yas)
+        fig, ax = plt.subplots()
+        ax.plot(xas, yas)
 
-    ax.set(xlabel='value', ylabel='steps',
-           title='Collatz')
-    ax.grid()
+        ax.set(xlabel='value', ylabel='steps',
+               title='Collatz')
+        ax.grid()
 
-    fig.savefig("collatz%s.png")
-    plt.show()
+        fig.savefig(os.path.join(COLLATZ_PATH, "collatz%s.png" % item))
+        plt.close(fig)
+        # plt.show()
 
 
 def max_steps():
@@ -71,25 +61,10 @@ def max_steps():
             print("Value: %15s -> Steps: %s" % (x, max))
 
 
-def collatz_plotter(min_value=1, max_value=100, dx=1):
-    xas = np.arange(min_value, max_value, dx)
-    yas = [collatz(x) for x in range(min_value, max_value)]
-
-    fig, ax = plt.subplots()
-    ax.plot(xas, yas)
-
-    ax.set(xlabel='value', ylabel='steps',
-           title='Collatz')
-    ax.grid()
-
-    fig.savefig("collatz.png")
-    plt.show()
-
-
 if __name__ == '__main__':
-
-    for x in collatz(3):
-        print(x)
+    collatz_plotter()
+    # for idx, x in enumerate(collatz(3), start=1):
+    #     print(idx, x)
 
     # print(collatz(775))
     # print(collatz(763))
